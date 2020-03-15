8a1a4777b27ee9bbd9e190277ef9127f4d4b5033
ahmedaak88 <ahmedaak88@gmail.com>
2020-03-15 18:33:50 +0300

    added links to the about page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-	return HttpResponse("Rango say hey there partner")
+	return HttpResponse("Rango say hey there partner <a href='/rango/about/'>About</a>")
 def about(request):
-	return HttpResponse("About page")
+	return HttpResponse("About page <a href='/rango/'>Index</a>")
 	pass
