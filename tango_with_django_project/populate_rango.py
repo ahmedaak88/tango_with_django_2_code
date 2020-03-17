79acfd6d452a7bffc4b5bfe9d57b7f3ec99a3754
ahmedaak88 <ahmedaak88@gmail.com>
2020-03-17 21:40:32 +0300

    trying to push exercise

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -31,16 +31,17 @@ def populate():
 		'url':'http://bottlepy.org/docs/dev/'},
 		{'title':'Flask',
 		'url':'http://flask.pocoo.org'} ]
-	cats = {'Python': {'pages': python_pages},
-			'Django': {'pages': django_pages},
-			'Other Frameworks': {'pages': other_pages} }
+	cats = {'Python': {'pages': python_pages, 'views':33, 'likes':3},
+			'Django': {'pages': django_pages, 'views':322,'likes':44},
+			'Other Frameworks': {'pages': other_pages,'views':0 ,'likes':0}}
 
 	# If you want to add more categories or pages,
 	# add them to the dictionaries above.
 	# The code below goes through the cats dictionary, then adds each category,
 	# and then adds all the associated pages for that category.
+	
 	for cat, cat_data in cats.items():
-		c = add_cat(cat)
+		c = add_cat(cat,cat_data['views'],cat_data['likes'])
 		for p in cat_data['pages']:
 			add_page(c, p['title'], p['url'])
 
@@ -51,8 +52,10 @@ def add_page(cat, title, url, views=0):
 	p.save()
 	return p
 
-def add_cat(name):
+def add_cat(name, views=0 , likes=0):
 	c = Category.objects.get_or_create(name=name)[0]
+	c.views = views
+	c.likes = likes 
 	c.save()
 	return c
 
